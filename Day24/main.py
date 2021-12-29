292e2be7ba3d678732b6e79547cf89acaddb1a9f
ljfhendriks <l.j.f.hendriks@student.tudelft.nl>
2021-12-29 13:04:43 +0100

    Attempt at day 24

--- Day24/main.py
+++ Day24/main.py
@@ -1,4 +1,6 @@
 from tqdm import tqdm
+import numpy as np
+
 
 def get_input(filename="input.txt"):
     with open(filename, "r") as file:
@@ -26,60 +28,95 @@ class ALU:
     def search_lowest(self):
         for operation in tqdm(self.operations):
             new_states = {}
-            match operation[0]:
-                case "inp":
-                    for state, model_number in self.states.items():
-                        for i in range(1, 10):
-                            new_state = list(state)
-                            new_state[letter_2_number[operation[1]]] = i
-                            new_state = tuple(new_state)
-                            if int(model_number + str(i)) > int(new_states.get(new_state, 0)):
-                                new_states[new_state] = model_number + str(i)
-                case "add":
-                    for state, model_number in self.states.items():
-                        new_state = apply_operation(state, operation, lambda x, y: x + y)
-                        if int(model_number) > int(new_states.get(new_state, 0)):
-                            new_states[new_state] = model_number
-                case "mul":
-                    for state, model_number in self.states.items():
-                        new_state = apply_operation(state, operation, lambda x, y: x * y)
-                        if int(model_number) > int(new_states.get(new_state, 0)):
-                            new_states[new_state] = model_number
-                case "div":
-                    for state, model_number in self.states.items():
-                        new_state = apply_operation(state, operation, lambda x, y: x // y)
-                        if int(model_number) > int(new_states.get(new_state, 0)):
-                            new_states[new_state] = model_number
-                case "mod":
-                    for state, model_number in self.states.items():
-                        new_state = apply_operation(state, operation, lambda x, y: x % y)
-                        if int(model_number) > int(new_states.get(new_state, 0)):
-                            new_states[new_state] = model_number
-                case "eql":
-                    for state, model_number in self.states.items():
-                        new_state = apply_operation(state, operation, lambda x, y: int(x == y))
-                        if int(model_number) > int(new_states.get(new_state, 0)):
-                            new_states[new_state] = model_number
+            if operation[0] == "inp":
+                for state, model_number in self.states.items():
+                    for i in range(1, 10):
+                        new_state = list(state)
+                        new_state[letter_2_number[operation[1]]] = i
+                        new_state = tuple(new_state)
+                        if int(model_number + str(i)) > int(new_states.get(new_state, 0)):
+                            new_states[new_state] = model_number + str(i)
+            elif operation[0] == "add":
+                for state, model_number in self.states.items():
+                    new_state = apply_operation(state, operation, lambda x, y: x + y)
+                    if int(model_number) > int(new_states.get(new_state, 0)):
+                        new_states[new_state] = model_number
+            elif operation[0] == "mul":
+                for state, model_number in self.states.items():
+                    new_state = apply_operation(state, operation, lambda x, y: x * y)
+                    if int(model_number) > int(new_states.get(new_state, 0)):
+                        new_states[new_state] = model_number
+            elif operation[0] == "div":
+                for state, model_number in self.states.items():
+                    new_state = apply_operation(state, operation, lambda x, y: x // y)
+                    if int(model_number) > int(new_states.get(new_state, 0)):
+                        new_states[new_state] = model_number
+            elif operation[0] == "mod":
+                for state, model_number in self.states.items():
+                    new_state = apply_operation(state, operation, lambda x, y: x % y)
+                    if int(model_number) > int(new_states.get(new_state, 0)):
+                        new_states[new_state] = model_number
+            elif operation[0] == "eql":
+                for state, model_number in self.states.items():
+                    new_state = apply_operation(state, operation, lambda x, y: int(x == y))
+                    if int(model_number) > int(new_states.get(new_state, 0)):
+                        new_states[new_state] = model_number
             self.states = new_states
+            print(len(self.states))
+
+    def search_highest(self):
+        i = 0
+        while i < len(self.operations):
+            new_states = {}
+            states = self.states.keys()
+            model_numbers = self.states.values()
+            states = np.array(list(states), dtype=int)
+            while self.operations[i][0] != "inp":
+                operation = self.operations[i]
+                state = apply_operation(states, operation, operation_dict[operation[0]])
+                i += 1
+            for state, model_number in zip(states, model_numbers):
+                for j in range(1, 10):
+                    state[letter_2_number[self.operations[i][1]]] = j
+                    new_state = tuple(state)
+                    if int(model_number + str(j)) > int(new_states.get(new_state, 0)):
+                        new_states[new_state] = model_number + str(j)
+            self.states = new_states
+            print(i)
+            print(len(self.states))
+            i += 1
 
 
-def apply_operation(state, operation, operator):
-    new_state = list(state)
+operation_dict = {
+    "add": lambda x, y: x + y,
+    "mul": lambda x, y: x * y,
+    "div": lambda x, y: x // y,
+    "mod": lambda x, y: x % y,
+    "eql": lambda x, y: (x == y).astype(int)
+}
+
+
+def apply_operation(states, operation, operator):
     if operation[2] in letter_2_number.keys():
-        new_state[letter_2_number[operation[1]]] = operator(new_state[letter_2_number[operation[1]]],
-                                                            new_state[letter_2_number[operation[2]]])
+        states[:, letter_2_number[operation[1]]] = operator(states[:, letter_2_number[operation[1]]],
+                                                            states[:, letter_2_number[operation[2]]])
     else:
-        new_state[letter_2_number[operation[1]]] = operator(new_state[letter_2_number[operation[1]]],
-                                                           int(operation[2]))
-    return tuple(new_state)
+        states[:, letter_2_number[operation[1]]] = operator(states[:, letter_2_number[operation[1]]],
+                                                            int(operation[2]))
+    return states
 
 
 def part1():
     alu = get_input()
 
-    alu.search_lowest()
+    alu.search_highest()
 
-    print(min([int(x) for x in alu.states.values()]))
+    max_model_number = 0
+    for state, model_number in alu.states.items():
+        if state[3] == 0 and int(model_number) > max_model_number:
+            max_model_number = int(model_number)
+
+    print(max_model_number)
 
 
 def part2():
